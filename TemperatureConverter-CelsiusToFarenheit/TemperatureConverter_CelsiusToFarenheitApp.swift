d98ddd336e2591a942e645d51f21d48696771591
Nathan Smith <nsmith@lcs.on.ca>
2021-12-02 09:56:53 -0500

    Initial Commit

---
+++ TemperatureConverter-CelsiusToFarenheit/TemperatureConverter_CelsiusToFarenheitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverter_CelsiusToFarenheitApp.swift
+//  TemperatureConverter-CelsiusToFarenheit
+//
+//  Created by Nate S on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverter_CelsiusToFarenheitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
