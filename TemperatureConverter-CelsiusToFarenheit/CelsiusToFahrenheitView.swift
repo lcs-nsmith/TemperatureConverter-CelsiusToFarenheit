3594f93e84ad69e7dd6d506a02cd766e783e0353
Nathan Smith <nsmith@lcs.on.ca>
2021-12-03 12:54:49 -0500

    Temperature Converter Done, was trying to figure the vetement suggestions by using a stored computed property but couldn't figure it out day nine class helped a lot

---
+++ TemperatureConverter-CelsiusToFarenheit/CelsiusToFahrenheitView.swift
@@ -0,0 +1,98 @@
+//
+//  CelsiusToFahrenheitView.swift
+//  TemperatureConverter-CelsiusToFarenheit
+//
+//  Created by Nate S on 2021-12-02.
+//
+
+import SwiftUI
+
+struct CelsiusToFahrenheitView: View {
+    
+    @State var temperatureInCelsius: Double = -6.5
+    
+    var temperatureInFahrenheit: Double {
+        return (temperatureInCelsius * 1.8)+36.0
+    }
+    
+    var vetementSuggestions: String {
+        switch temperatureInCelsius {
+        case ...(-50):
+            return "Don't go outside, It's too cold!"
+        case -49.5...(-30):
+            return "Dress as warmly as you possibly can!"
+        case -29.5...(-10):
+            return "Dress extremely warmly!"
+        case -9.5...0:
+            return "Put on a coat!"
+        case 0.5...10:
+            return "Wear a sweater or a hoodie!"
+        case 10.5...15:
+            return "Its the perfect temperature!"
+        case 15.5...20:
+            return "It's t-shirt weather!"
+        case 20.5...35:
+            return "Its hot outside!"
+        default:
+            return "Its really hot outside! Try to stay inside, but if you have to go out, stay in the shade!"
+        }
+    }
+    
+    var body: some View {
+        return VStack (alignment: .leading) {
+            Text("Celsius")
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            Slider(value: $temperatureInCelsius,
+                   in: -63.0...50.0,
+                   step: 0.5,
+                   label: {
+                Text("Celsius Slider")
+            },
+                   minimumValueLabel: {
+                Text("-63°C")
+                    .font(.system(size: 16, weight: .semibold, design: .rounded))
+            },
+                   maximumValueLabel: {
+                Text("50°C")
+                    .font(.system(size: 16, weight: .semibold, design: .rounded))
+            })
+            HStack {
+                Spacer()
+                Text("\(String(format: "%.1f", temperatureInCelsius))")
+                    .font(.system(size: 25, weight: .medium , design: .rounded))
+                Spacer()
+            }
+            Text("Fahrenheit")
+                .fontWeight(.bold)
+                .padding(.top)
+            
+            HStack {
+                Spacer()
+                Text("\(String(format: "%.1f", temperatureInFahrenheit))")
+                    .font(.system(size: 25, weight: .medium , design: .rounded))
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                Text(vetementSuggestions)
+                    .font(.system(size: 25, weight: .thin , design: .rounded))
+                Spacer()
+            }
+            .padding(.vertical)
+            Spacer()
+        }
+        .padding(.horizontal)
+        .navigationTitle("Temperature Converter")
+        
+    }
+}
+
+struct CelsiusToFahrenheitView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CelsiusToFahrenheitView()
+        }
+    }
+}
--- TemperatureConverter-CelsiusToFarenheit/TemperatureConverter_CelsiusToFarenheitApp.swift
+++ TemperatureConverter-CelsiusToFarenheit/TemperatureConverter_CelsiusToFarenheitApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TemperatureConverter_CelsiusToFarenheitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CelsiusToFahrenheitView()
         }
     }
 }
